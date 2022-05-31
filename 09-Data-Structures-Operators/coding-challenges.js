6c4daf23ae95a813bdd1fb72bf5fca8e9faa064d
Protick Roy <protickrr@gmail.com>
2022-06-01 04:32:26 +0600

    coding challenge modern data structure and operators

---
+++ 09-Data-Structures-Operators/coding-challenges.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const printGoals = function (...players) {
+    for(let i = 0; i < players.length; i++) {
+        console.log(players[i]);
+    }
+    console.log(`Number of goals: ${players.length}`);
+}
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = [...players1];
+const allPlayers = [ ...players1, ...players2 ];
+const players1Final = [ ...players1, 'Thiago', 'Coutinho', 'Perisic' ];
+// const {team1, x:draw, team2} = game.odds;
+const {odds: { team1, x: draw, team2 }} = game; 
+printGoals(...game.scored);
+
+team1 < team2 && console.log('team1 is more likely to win');
+team1 > team2 && console.log('team2 is more likely to win');
