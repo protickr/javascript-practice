0be52b6ffda998ab61254bb856107267fe5c8f88
Protick Roy <protickrr@gmail.com>
2022-06-09 15:56:03 +0600

    working with arrays upto coding challenge 1

---
+++ 11-Arrays-Bankist/coding-challenges.js
@@ -0,0 +1,27 @@
+'use strict';
+/*Coding challenge 1 
+
+function checkDogs(dogsJulia, dogsKate) {
+  let dogsJuliaCopy = dogsJulia.slice().slice(1, -2);
+
+  const printResult = cleanData => {
+    cleanData.forEach(function (dogsAge, i, arr) {
+      console.log(
+        `Dog number ${i + 1} is ${
+          dogsAge >= 3
+            ? `an adult 🐕, and is ${dogsAge} years old`
+            : `still a puppy 🐶`
+        } `
+      );
+    });
+  };
+
+  printResult(dogsJuliaCopy);
+  console.log('-'.repeat(30));
+  printResult(dogsKate);
+}
+
+const dataJulia = [3, 5, 2, 12, 7];
+const dataKate = [4, 1, 15, 8, 3];
+checkDogs(dataJulia, dataKate);
+*/
