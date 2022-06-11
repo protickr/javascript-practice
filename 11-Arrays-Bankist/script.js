fc8532db45d9f912b5a4daaf2bd2f5a51aa14915
Protick Roy <protickrr@gmail.com>
2022-06-11 17:58:53 +0600

    working with Arrays upto sort()

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -195,3 +195,14 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = '';
   inputClosePin.value = '';
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some((mov)=> mov >= amount * 0.1)){
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
