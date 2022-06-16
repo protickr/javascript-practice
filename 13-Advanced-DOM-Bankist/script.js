201f4dc1aa13358383bbd0d6f597385b43a0c530
Protick Roy <protickrr@gmail.com>
2022-06-17 00:13:52 +0600

    Notes updated on DOM methods

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,29 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'we use cookies for improved functionality and analytics <button class="btn btn--close-cookie"> Accept all</button>';
+header.append(message);
+
+// close cookie button event listener
+document.querySelector('.btn--close-cookie').addEventListener('click', function(e){
+  e.preventDefault();
+  // this.parentNode.remove();
+  message.remove();
+});
+
+
+// style the cookie message 
+message.style.backgroundColor = '#37383d';
+message.style.width = '98.8vw';
+console.log(getComputedStyle(message).height);
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+console.log(message.style.height);
+
+let logo = document.querySelector('.nav__logo');
+logo.alt = 'Beautiful Minimalist Logo';
