767408ece104915bd3698d958cf7044e9e14da05
Protick Roy <protickrr@gmail.com>
2022-06-18 00:03:04 +0600

    Notes upto Event Delegation and Bankist webpage page navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+let btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const header = document.querySelector('.header');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,13 +36,17 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
-
-const header = document.querySelector('.header');
+// create and add element to the DOM 
+// create Cookie message element
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML = 'we use cookies for improved functionality and analytics <button class="btn btn--close-cookie"> Accept all</button>';
 header.append(message);
+
+// style the cookie message 
+message.style.backgroundColor = '#37383d';
+message.style.width = '98.8vw';
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
 // close cookie button event listener
 document.querySelector('.btn--close-cookie').addEventListener('click', function(e){
@@ -47,23 +55,19 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
   message.remove();
 });
 
-
-// style the cookie message 
-message.style.backgroundColor = '#37383d';
-message.style.width = '98.8vw';
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
+// changing alt text of bankist logo 
 let logo = document.querySelector('.nav__logo');
 logo.alt = 'Beautiful Minimalist Logo';
-let btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-const section1Coords = section1.getBoundingClientRect();
 
+// smooth scrolling for
 // 'learn more' button's click event handler function
 btnScrollTo.addEventListener('click', function (e) {
   e.preventDefault();
+    // new way 
+    section1.scrollIntoView({behavior: 'smooth'});
 
   /*
+  const section1Coords = section1.getBoundingClientRect();
   window.scrollTo(
     Number.parseFloat(section1Coords.left + window.pageXOffset),
     Number.parseFloat(section1Coords.top + window.pageYOffset)
@@ -79,27 +83,52 @@ btnScrollTo.addEventListener('click', function (e) {
     behavior: 'smooth',
   });
   */
-
-  // new way 
-  section1.scrollIntoView({behavior: 'smooth'});
 });
 
 // adding event listener 
-
-// 1. 
-// more modern than 'on event property' 
-// also its advantages over to the prior is that we can add multiple 
-// event handler to same event for an element whereas 
-// if we try to do the same with the 'on event property' then the later event handler
-// overwrites the previous event handler function 
-// also we can remove event listener from an element by removeEventListener('event', functionReference);
-// to remove a event handler from an element we need to attach a named function first
+/*
 let h1 = document.querySelector('h1');
 h1.addEventListener('mouseenter', function(){
   console.log('You are reading heading');
 });
-
+*/
 // 2.
-h1.onmouseenter = function () { console.log('added event listener via on event property, onmouseenter')};
+// h1.onmouseenter = function () { console.log('added event listener via on event property, onmouseenter')};
+/*
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
-// 3. 
+const changeColor = function (e) {
+  this.style.backgroundColor = randomColor(); 
+  e.stopPropagation();
+};
+
+  document.querySelector('.nav__link').addEventListener('click', changeColor);
+  document.querySelector('.nav__links').addEventListener('click', changeColor);
+  document.querySelector('.nav').addEventListener('click', changeColor);
+*/
+
+
+// page navigation 
+/* Normal solution 
+const navLinks = document.querySelectorAll('.nav__link');
+navLinks.forEach(function (item, index, wholeCollection) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  });
+});
+*/
+/* Event Delegation Solution */
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  e.preventDefault();
+  
+  // matching stratagy 
+  if (e.target.classList.contains('nav__link')){
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+});
