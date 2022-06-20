fe00cfa3c3b4289e87dfe6cdcbd525e500215777
Protick Roy <protickrr@gmail.com>
2022-06-20 23:21:19 +0600

    Notes upto prototype chainand OOP coding challenge 1

---
+++ 14-OOP/coding-challenges.js
@@ -0,0 +1,29 @@
+'use strict';
+/* Coding challenge 1 */
+
+const Car = function (make, speed) {
+  this.make = make; 
+  this.speed = speed; // km/h
+};
+
+Car.prototype.displayCurrentSpeed = function () {
+  console.log(`Current Speed of ${this.make}: ${this.speed} km/h`);
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10; 
+  this.displayCurrentSpeed();
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5; 
+  this.displayCurrentSpeed();
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.brake(); 
+car1.brake();
+car2.accelerate();
+car2.accelerate();
--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,30 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName; 
+    this.birthYear = birthYear;
+
+    // defining function inside a constructor function is not a good practice
+    /*
+        this.calcAge = function (currYear){
+            return currYear - this.birthYear;
+        };
+    */
+};
+
+Person.prototype.calcAge = function (currYear){
+    return currYear - this.birthYear;
+};
+
+// prototypes
+const protick = new Person('Protick', 1994);
+// console.log(Person.prototype);
+// console.log(protick.calcAge(2022));
+// const h1 = document.querySelector('h1');
+// console.dir(h1);
+
+/*
+function someFunction(){
+    console.log('some function');
+};
+*/
+// console.dir(someFunction);
