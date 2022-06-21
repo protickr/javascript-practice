ef772fd88516034c52840873793dae2f37901a02
Protick Roy <protickrr@gmail.com>
2022-06-22 04:24:55 +0600

    leaflet library added and coords from geolocation browser api

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap',
+      }).addTo(map);
+
+      var marker = L.marker(coords)
+        .addTo(map)
+        .bindPopup('CSS3 Popup <br> Easily Customizable')
+        .openPopup();
+    },
+
+    function () {
+      alert('Could not get current position');
+    }
+  );
+}
