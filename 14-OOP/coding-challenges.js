75d9eb0d75b803b300ac3c432858ab6a7ed009a2
Protick Roy <protickrr@gmail.com>
2022-06-22 01:35:41 +0600

    WEIRD OOP OF JS coding challenges and notes updated

--- 14-OOP/coding-challenges.js
+++ 14-OOP/coding-challenges.js
@@ -29,3 +29,79 @@ car2.accelerate();
 car2.accelerate();
 */
 
+/* Coding challenge 2 
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  displayCurrentSpeed() {
+    console.log(`Current Speed of ${this.make}: ${this.speed} km/h`);
+  }
+
+  accelerate() {
+    this.speed += 10;
+    this.displayCurrentSpeed();
+  }
+
+  brake() {
+    this.speed -= 5;
+    this.displayCurrentSpeed();
+  }
+
+  get speedUS() {
+    const spdus = this.speed / 1.6;
+    console.log(`Current Speed of ${this.make}: ${spdus} Mph`);
+    return spdus;
+  }
+
+  set speedUS(speedInMph) {
+    this.speed = speedInMph * 1.6;
+  }
+}
+
+const car1 = new CarCl('Ford', 120);
+console.log(car1.speedUS);
+*/
+
+/* Coding challenge 3 
+const Car = function (make, speed) {
+  this.make = make; 
+  this.speed = speed; // km/h
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10; 
+  console.log(`Current Speed of ${this.make}: ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5; 
+  console.log(`Current Speed of ${this.make}: ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge){
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV; 
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo; 
+  console.log(`${this.make} charged to ${this.charge}%`);
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20; 
+  this.charge -= 1; 
+  console.log(`${this.make} going at ${this.speed} km/h with a charge of ${this.charge}%`);
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+*/
--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -73,7 +73,7 @@ console.log(protick.age);
 console.log(protick.fullName);
 Person.hey();
 */
-
+/*
 // prototypal inheritance using Object.crete(); 
 // we can set the prototype manually for an object 
 
@@ -96,3 +96,198 @@ const protick = Object.create(PersonProto);
 // initializing 
 protick.init('Protick', 1994);
 console.log(protick.calcAge());
+*/
+
+
+
+
+
+
+
+
+// inheritance between classes
+
+// using constructor function 
+/*
+// ------------------ Person -------------------
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName; 
+    this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function (){
+    return new Date().getFullYear() - this.birthYear;
+};
+
+// ------------------ Student -------------------
+const Student = function (firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student; 
+
+// or this ? 
+// Student.prototype.__proto__ = Person.prototype; 
+
+Student.prototype.introduce = function () {
+  console.log(` I'm ${this.firstName}, studying ${this.course}`);
+};
+
+// ------------------ Objects -------------------
+const trevor = new Student('Trevor Noah', 1991, 'Computer Science'); 
+console.log(trevor.calcAge());
+trevor.introduce();
+console.dir(trevor);
+*/
+
+// using ES6 class - extends and super()
+/*
+// ------------------ Person -------------------
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge(currYear) {
+    return currYear - this.birthYear;
+  }
+
+  get age() {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else console.log('Provide your Full name');
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log(`Static method / helper function`);
+  }
+}
+
+class Student extends Person {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(` I'm ${this.fullName}, studying ${this.course}`);
+  }
+}
+
+const protick = new Student('Protick Roy', 1994, 'Computer Science');
+console.log(protick.calcAge(2022));
+console.log(protick.age);
+console.log(protick.fullName);
+protick.introduce();
+*/
+
+/*
+// Using Object.create()
+const PersonProto = {
+  calcAge() {
+    return new Date().getFullYear() - this.birthYear;
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function(firstName, birthYear, course){
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+StudentProto.introduce = function () {
+    console.log(` I'm ${this.firstName}, studying ${this.course}`);
+}; 
+
+// initializing
+const jay = Object.create(StudentProto);
+jay.init('Jay Williams', 1994, 'Computer Science');
+jay.calcAge();
+jay.introduce();
+*/
+
+// another class example 
+// 1. Public field
+// 2. Private field
+// 3. Public method
+// 4. Private method 
+// 5. - 8. (There are also 'static' version of this four types as well)
+
+
+class Account {
+  // public fields (instance properties not prototype's)
+  // 1. Public fields
+  locale = navigator.language;
+
+  // private fields (instance properties not prototype's)
+  // 2. Private fields 
+  #pin;
+  #movements = [];
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    this.#movements = [];   
+    // Protected property
+    // this._pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  // APIs / accessor functions
+  // Public Interface of our objects
+  // 3. Public method
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(value) {
+    this.#movements.push(value);
+    return this;
+  }
+
+  // abstraction : that withdraw is a negative movement
+  withdraw(value) {
+    this.deposit(value * -1);
+    return this;
+  }
+
+  // encapsulatation of protected method: that should not be accessed from outside of the class as per convention
+  // _approveLoan(val) {
+  //   return true;
+  // }
+
+  // 4. Private method 
+  // private method on the instance itself not on the object's prototype's
+  #approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+    }
+    return this;
+  }
+}
+
+const accProtick = new Account('Protick Roy', 'BDT', 1234);
+console.log(accProtick.deposit(100).withdraw(50).requestLoan(1000).getMovements());
